6e01a896d9f538dc4b74694458171091757c39f8
ddevna <32869712+ddevna@users.noreply.github.com>
2024-08-22 21:57:04 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def get_requirements(file_path:str)->List[str]:
     return requirements"""
 
 setup(
-    name='DiamondPricePrediction',
+    name='DimondPricePrediction',
     version='0.0.1',
     author='deepak devna',
     author_email='deepakdevna@gmail.com',
